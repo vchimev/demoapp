3f150d700b79268c541a1b1627ad24d1fdc3e6bc
Vasil Chimev <Vasil.Chimev@gmail.com>
2018-05-24 19:53:25 +0300

    Angular Basics 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+class HairstyleItem {
+  location: string;
+  price: number;
+  hairstyle: string;
+  picture: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  items: HairstyleItem[] = [{
+    location: 'Madrid',
+    price: 290,
+    hairstyle: 'Cousin It',
+    picture: 'assets/images/hirez.png'
+  },
+  {
+    location: 'Copenhagen',
+    price: 361,
+    hairstyle: 'Boom Fox',
+    picture: 'assets/images/hirez.png'
+  }];
 }
